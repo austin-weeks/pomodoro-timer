3e0af53155e39f74eb792983ba1f626fb15b9d6d
Austin Weeks <168379071+austin-weeks@users.noreply.github.com>
2024-05-29 19:34:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pomodoro Timer
+This is a project built for [freeCodeCamp](https://www.freecodecamp.org/learn/)'s Front End Development Libraries Course.
+
+View my other projects at my [portfolio](https://austin-weeks.github.io).
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='heading'>
+        <h1>Pomodoro Timer</h1>
+      </div>
+
+      <div style={{ fontSize: "0.8rem", marginTop: ".8em" }}>
+        made by <a href="http://austin-weeks.github.io" target="_blank">austin weeks</a>
+      </div>
+    </>
+  )
+}
+
+export default App
